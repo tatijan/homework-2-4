a61bd10bfde0b178a0dd92e4e807eba4b0af351d
tatijan <34741441+tatijan@users.noreply.github.com>
2019-01-16 23:31:44 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!$_SERVER['PHP_AUTH_USER']) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    http_response_code(401);
-    die ("Not authorized");
-} else {
-    header("Location: list.php");
-}
